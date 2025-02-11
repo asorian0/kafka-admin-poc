48da72e3805fe8a7f3892bc1c2b4cf7b0dc807ba
Armando Soriano <asoriano.dev@gmail.com>
2025-02-11 15:50:03 +0100

    chore(admin-client): base code

---
+++ admin-client/src/test/java/dev/asoriano/kafkaadminpoc/KafkaAdminPocApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.asoriano.kafkaadminpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaAdminPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
